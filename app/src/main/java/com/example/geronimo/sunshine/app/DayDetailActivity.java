a086185b48a8505df86c48b9da34a661680fe8cb
Geronimo Romney <g.romney@gmail.com>
2015-02-18 14:02:02 -0400

    agregar un Activity: detalles del dia

---
+++ app/src/main/java/com/example/geronimo/sunshine/app/DayDetailActivity.java
@@ -0,0 +1,20 @@
+package com.example.geronimo.sunshine.app;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.MenuItem;
+
+/**
+ * Created by Geronimo on 2/18/2015.
+ */
+public class DayDetailActivity extends ActionBarActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+}
